04d2b6e14f0bc4aa8b22d7a2c9d4052f67775d8d
Stanley Fils <stanleyfils@gmail.com>
2020-05-30 20:35:11 -0400

    Study alternative methods and syntax

--- Where-my-anagrams-at?.js
+++ Where-my-anagrams-at?.js
@@ -7,7 +7,7 @@ anagrams = (string, words) => {
 
 anagrams("racer", ["crazer", "carer", "racar", "caers", "racer"]);
 
-// alternative methods
+// alternative syntax
 // const anagrams = function(string, array) {
 //   return array.filter(function(word) {
 //     return word.split("").sort().join("") === string.split("").sort().join("");
@@ -23,3 +23,31 @@ anagrams("racer", ["crazer", "carer", "racar", "caers", "racer"]);
 // }
 
 // anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer'])
+
+// Alternative methods
+
+// function anagrams(word, words) {
+//   word = word.split('').sort().join('');
+//   return words.filter(function(v) {return word == v.split('').sort().join('');});
+// }
+
+// =-=-=-=-=-=-=-=-=-=-=-=-
+// let anagrams = (word, words) => words.filter(w => w.split('').sort().join('') === word.split('').sort().join(''));
+
+// =-=-=-=-=-=-=-=-=-=-=-=-
+// function anagrams(word, words) {
+//   return words.filter(function (e) {
+//       return e.split('').sort().join('') === word.split('').sort().join('');
+//   })
+// }
+
+// =-=-=-=-=-=-=-=-=-=-=-=-
+// String.prototype.sort = function() {
+//   return this.split("").sort().join("");
+// };
+
+// function anagrams(word, words) {
+//   return words.filter(function(x) {
+//       return x.sort() === word.sort();
+//   });
+// }
