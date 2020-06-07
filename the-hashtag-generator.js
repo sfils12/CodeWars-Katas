dd71367ee9d761f35460247721b8914b3fd3605f
Stanley Fils <stanleyfils@gmail.com>
2020-06-07 00:01:09 -0400

    Update bug fix details

--- the-hashtag-generator.js
+++ the-hashtag-generator.js
@@ -14,10 +14,10 @@ function generateHashtag(string) {
   return hashtagString.length > 140 ? false : hashtagString;
 }
 
-//   Wasn't working at first. Here is what I changed to fix it.
+//   Wasn't working at first. Here is what I changed to fix it. I think it was reading an empty string as a string and concatenating it to the #.
 
 // This is probably happening since the test case is an empty string (empty string meaning a string with any number of spaces, but no alphanumeric characters)
 
 // It's passing ' ' (or something similar) to the function and the split is splitting the string into ["", ""] which doesn't have any characters in each index.
 
-// In order to prevent this, you'll want to add string = str.trim() at the start of the function to ensure that those edge cases don't slip through the cracks.
+// In order to prevent this, add string = str.trim() at the start of the function to ensure that those edge cases don't slip through the cracks.
